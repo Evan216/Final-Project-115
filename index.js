880d93dbe4de1b7170964d616451806eaba682de
Evan <erhaldenan221@stevenscollege.edu>
2022-11-11 09:35:17 -0500

    Version 3

---
+++ index.js
@@ -0,0 +1,15 @@
+let x = document.body.getElementsByClassName("btn3")
+let y =[]
+function superAwesomeFunky(blade){
+    if (string.length(document.getElementById("sig").innerHTML) <= 8) {
+        let Username = blade.parentElement.children[1].value
+        let Password = blade.parentElement.children[2].value
+    } else {
+        alert("Please enter a proper username")
+    }
+   
+window.onload = function() {
+    localStorage.setItem("name", $('#inputName').val());
+    localStorage.setItem("email", $('#inputEmail').val());   
+}
+} 
