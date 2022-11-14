485b7bef13fe3f92e7c05882feca4e942dc83624
Evan <erhaldenan221@stevenscollege.edu>
2022-11-14 08:05:29 -0500

    Version 4

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-let x = document.body.getElementsByClassName("btn3")
-let y =[]
-function superAwesomeFunky(blade){
-    if (string.length(document.getElementById("sig").innerHTML) <= 8) {
-        let Username = blade.parentElement.children[1].value
-        let Password = blade.parentElement.children[2].value
-    } else {
-        alert("Please enter a proper username")
+// page
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
-   
-window.onload = function() {
-    localStorage.setItem("name", $('#inputName').val());
-    localStorage.setItem("email", $('#inputEmail').val());   
 }
-} 
